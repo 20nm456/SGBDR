e69280448708c8f0499949a35cb4f1c2976e0ebc
20nm456 <168471403+20nm456@users.noreply.github.com>
2024-07-13 00:30:18 +0200

    Add files via upload

---
+++ sgbdr/bdd.h
@@ -0,0 +1,15 @@
+#ifndef BDD_H_INCLUDED
+#define BDD_H_INCLUDED
+
+typedef struct bdd bdd
+struct bdd
+{
+    int taille
+};
+
+{
+    /* data */
+};
+
+
+#endif
---
+++ sgbdr/table.h
@@ -0,0 +1,10 @@
+#ifndef TABLE_H_INCLUDED
+#define TABLE_H_INCLUDED
+
+typedef struct table table
+struct table{
+    int idtable
+};
+
+
+#endif // TABLE_H_INCLUDED
